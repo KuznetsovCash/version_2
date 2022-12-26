0d01b6289b29c439d1d674be69fe0760d139e35a
KuznetsovCash <kuznetsovcash@gmail.com>
2022-12-26 22:50:38 +0300

    app.py chang2d3dfevv]

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title('BTC')
 
 test = st.file_uploader("Choose a CSV file", type=['csv'])
 if not test is None: 
-       data = pd.read_csv(test)   
+       data = pd.read_csv(test, sep=';').drop(columns=['<DATE>', '<TIME>'])   
        result = process(data)
        st.write(result)
 
