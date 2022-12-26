9b53951540e93a2285c7daee3635e157be125b85
KuznetsovCash <kuznetsovcash@gmail.com>
2022-12-26 22:30:12 +0300

    app.py chang2e]

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from segment import process
 
 st.title('BTC')
 
-test = st.file_uploader("Choose a CSV file")
+test = st.file_uploader("Choose a CSV file", type=['csv'])
 file_r = test.read()
 
 process(file_r)
