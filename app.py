0cdd61065245644ca166dc0a56e6d95077e7d3e3
KuznetsovCash <kuznetsovcash@gmail.com>
2022-12-26 22:44:33 +0300

    app.py chang2d3e]

--- app.py
+++ app.py
@@ -7,11 +7,10 @@ import pandas as pd
 st.title('BTC')
 
 test = st.file_uploader("Choose a CSV file", type=['csv'])
-data = pd.read_csv(test)
-
-process(data)
-
-
+if not test is None: 
+       data = pd.read_csv(test)   
+       result = process(data)
+       st.write(result)
 
 
 
