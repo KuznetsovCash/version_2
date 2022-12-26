cfb9bed27a1bae799d86cafeb1ff1460a29ac29b
KuznetsovCash <kuznetsovcash@gmail.com>
2022-12-26 22:51:37 +0300

    app.py chang2d3dfevfv]

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ test = st.file_uploader("Choose a CSV file", type=['csv'])
 if not test is None: 
        data = pd.read_csv(test, sep=';').drop(columns=['<DATE>', '<TIME>'])   
        result = process(data)
-       st.write(result)
+       print(st.write(result))
 
 
 
